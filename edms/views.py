ebd7cce30f34c5ad6249f3ee7d70cc8808b9ac4d
Sindgein <Fuermohao@outlook.com>
2018-03-24 17:49:50 +0800

    fixed: {{ }}

--- edms/views.py
+++ edms/views.py
@@ -1,24 +1,27 @@
 # -*- coding:utf-8 -*-
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponse,FileResponse,StreamingHttpResponse,JsonResponse
+from django.http import HttpResponse, FileResponse, StreamingHttpResponse, JsonResponse
 # Create your views here.
-def edms(request):
-    user_id = request.user.username
-    user_name = request.user.teacher.name
 
-    return render(request,'edms/index.html',locals())
+
+def edms(request):
+    content = {
+        'user_id': request.user.username,
+        'user_name': request.user.teacher.name
+    }
+    return render(request, 'edms/index.html',context=content)
 
 
 def file_download(request):
     # do something...
     a = MyModel.objects.all()[2]
     paths = a.upload.url
-    
-    with open(paths,'rb') as f:
+
+    with open(paths, 'rb') as f:
         c = f.read()
-    
-    response = HttpResponse(c)        
+
+    response = HttpResponse(c)
     response['Content-Type'] = 'application/octet-stream'
     response['Content-Disposition'] = 'attachment;filename="test.xlsx"'
     return response
